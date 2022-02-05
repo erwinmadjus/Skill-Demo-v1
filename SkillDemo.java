bc278662c633bd45945838d64009e4d425839a64
Erwin Madjus <erwinmadjusbusiness@gmail.com>
2022-02-04 16:48:10 -0800

    Lib + failed code

--- SkillDemo.java
+++ SkillDemo.java
@@ -1,1 +1,11 @@
-public class SkillDemo {}
+public class SkillDemo {
+    public static int add(int n, int m) {
+        if (n > m) {
+            return n + m;
+        else if (n < m) {
+            return m - n;
+        } 
+        else return 0;
+        }
+}
+}
